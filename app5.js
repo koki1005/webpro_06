75d4f0266a37d2aa2f6a5bd1f775f5f67fedf131
Koki Sano <koki.sano1005@gmail.com>
2024-11-18 20:55:09 +0900

    なぞなぞと簡易的な電卓の機能を追加し，app5.jsの説明をした

--- app5.js
+++ app5.js
@@ -21,27 +21,29 @@ app.get("/icon", (req, res) => {
 });
 
 app.get("/luck", (req, res) => {
-  const num = Math.floor( Math.random() * 6 + 1 );
+  const num = Math.floor(Math.random() * 6 + 1);
   let luck = '';
-  if( num==1 ) luck = '大吉';
-  else if( num==2 ) luck = '中吉';
+  if (num == 1) luck = '大吉';
+  else if (num == 2) luck = '中吉';
+  else if (num == 3) luck = '吉';
+  else if (num == 4) luck = '凶';
+  else if (num == 5) luck = '小吉';
+  else if (num == 6) luck = '大凶';
   console.log( 'あなたの運勢は' + luck + 'です' );
   res.render( 'luck', {number:num, luck:luck} );
 });
 
 app.get("/janken", (req, res) => {
-  let hand = req.query.hand;  // プレイヤーの手（クエリパラメータから取得）
-  let win = Number(req.query.win);  // プレイヤーの勝利数
-  let total = Number(req.query.total);  // 総試合数
+  let hand = req.query.hand;  
+  let win = Number(req.query.win);  
+  let total = Number(req.query.total);  
 
-  // コンピュータの手をランダムに決定
   const num = Math.floor(Math.random() * 3 + 1);
   let cpu = '';
   if (num == 1) cpu = 'グー';
   else if (num == 2) cpu = 'チョキ';
   else cpu = 'パー';
 
-  // 勝敗の判定
   let judgement = '';
   if (hand === cpu) {
     judgement = '引き分け';
@@ -56,9 +58,8 @@ app.get("/janken", (req, res) => {
     judgement = '負け';
   }
 
-  total += 1;  // 試合数をカウントアップ
+  total += 1;  
 
-  // 表示用のデータをオブジェクトに格納
   const display = {
     your: hand,
     cpu: cpu,
@@ -80,7 +81,7 @@ app.get('/nazonazo', (req, res) => {
     if (userAnswer === correctAnswer) {
       resultMessage = '正解！よくできました！';
     } else {
-      resultMessage = '残念、不正解です。';
+      resultMessage = '残念，不正解です．';
     }
   }
 
